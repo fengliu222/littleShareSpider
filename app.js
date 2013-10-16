c4f91d8d4ffd0605d693f223558e28bc40c88f23
Moe <444984@qq.com>
2013-10-17 00:39:32 +0800

    封装好了可以拿出去见人版。

--- app.js
+++ app.js
@@ -3,58 +3,56 @@ var che = require("cheerio");
 var fs = require('fs');
 
 var Catch = new urls({
-						rootUrl:"http://littleshar.com/",
-						listUrl:"board/message/sort/favorite/page/"
-			});
-			
-Catch.next(getSinglePageList);
+	rootUrl: "http://www.xunl.us/",
+	listUrl: "tf"
+});
 
-//处理得到的单页数据。
-function getSinglePageList(data){
+Catch.itemHandle = function() {
+	Catch.getData(Catch.menuList[Catch.totalPage], function(data) {
+		console.log("第"+ (Catch.totalPage+1) +"页");
+		var $ = che.load(data);
+		if($("a[target='kuaichuan']")[0]){
+			var target = $("a[target='kuaichuan']")[0].attribs.href;
+			Catch.getData(target, function(res){
+				var $ = che.load(res);
+				var finalTarget = $('.player a')[0].attribs.href;
+				writeFile(finalTarget, "res.txt");
+				Catch.totalPage++;
+				Catch.next();
+ 
+			})
+		}else{
+			Catch.totalPage++;
+			Catch.next();
+		}
+	})
+}
+
+Catch.init(function(data){
 	var $ = che.load(data);
-	var list = $(".pin a[class='cover']");
- 	var result = [];
- 	 console.log(list.length);
-	for(var i=0; i < list.length; i++){
-		result.push(list[i].attribs['href']);
-	}
- 	pageParser(result);
-	 
+	var listTemp = $(".message1 a");
+	listTemp.each(function(index, el) {
+		Catch.menuList.push(Catch.rootUrl + el.attribs.href + " ");
+	})
+	console.log(Catch.menuList.length)
+	Catch.next(); 
+});
+
+
+
+
+function writeFile(content, fileName) {
+	var gs = content + ":\n\n";
+	fs.stat(fileName, function(err, stats) {
+		if (stats == undefined) {
+			fs.open(fileName, "w", function() {
+				fs.appendFileSync(fileName, gs + "\n\n");
+			});
+
+		} else {
+
+			fs.appendFileSync(fileName, gs + "\n\n");
+
+		}
+	})
 }
-
-var curPageIndex = 0;
-function pageParser(res){
-	if(curPageIndex < res.length){
-		Catch.getData(Catch.rootUrl+res[curPageIndex],function(data){
-			 
-			var $ = che.load(data);
-			var title = $(".message-img h3").html();
-			var href = Catch.rootUrl + $(".action a").eq(0)[0].attribs['href'];
- 			
- 			writeF(title,href);
-			curPageIndex++;
-			pageParser(res);
-		})
-	}else{
-		curPageIndex=0;
-		Catch.page++;
-		Catch.next(getSinglePageList);
-	}
-}
-
-function writeF(title,href){
-	 var gs = title + ":\n\n" + href;
-	 fs.stat("result.txt", function(err,stats){
-							if(stats == undefined){
-								 fs.open("result.txt","w",function(){
-								 	fs.appendFileSync("result.txt",gs+"\n\n");
-								 });
-
-							}else{
-
-								fs.appendFileSync("result.txt",gs+"\n\n");
-
-							}
-						})
-}
-					
--- bug.js
+++ bug.js
@@ -1,8 +1,28 @@
-/**
- * @author moejser
- * 抓取器适用于有一个列表页的站点，通过获取列表页中的A标签属性来批量抓取信息。
- * urls函数只提供数据抓取的工作，不应该与数据处理函数耦合。
- */
+/*
+	使用方法：
+		首先实例化this
+		var foo = new urls({
+			rootUrl : "", //域名
+			lsitUrl : "", //列表页地址，相对路径
+		})
+	
+		调用init方法，init返回列表页dom，需要手动调用cheerio API解析DOM.
+		foo.init(function(listDom){
+			... //解析逻辑
+			... //将列表页的每一项的地址存入foo.menuList中，这一项是必须的，因为hasNext方法依赖此变量进行遍历。
+			foo.next(); //开始解析，此处是递归调用itemHandle方法，数据具体的处理逻辑需要在itemHandle方法中实现。
+
+		})
+		
+		foo.itemHandle = function(data){
+			...从列表页进入内容页的逻辑，也许进入的是另外一个列表页，但是已经由多对一变成一对一的访问，使用getData方法进行处理即可。
+			foo.totalPage++; //增加遍历索引
+			foo.next(); //下一个Item
+		}
+
+	})
+
+*/
 
 var http = require("http");
 
@@ -15,29 +35,26 @@ var urls = function(options,callback){
 	this.listUrl = options.listUrl.search(/^\//) != -1 ? options.listUrl.slice(1) : options.listUrl || "";
 	this.page = options.startIndex || 1;
 	this.listDom = options.listDom || "";
-	this.totalPage = "";
+	this.totalPage = 0;
 	this.menuList = new Array(); 
 
 }
-
-urls.prototype.next = function(callback){
- 	callback = callback || {};
- 	console.log("已经下载到第"+this.page+"页");
-	if(this.hasNext){
-		this.getData(this.rootUrl+this.listUrl+this.page,function(data){
-			 
-			callback(data);
-		})
-	}else{
-		console.log("已经全部下载完毕");
-		return;
-	}	 
-	
-	
+urls.prototype.init = function(callback){
+	this.getData(this.rootUrl + this.listUrl, function(data){
+		callback(data);
+	})
 }
+
+urls.prototype.next = function(){
+	var self = this;
+	this.hasNext(function() { 
+		self.itemHandle();
+	})
+}
+
 urls.prototype.hasNext = function(callback){
- 	if(this.page<51){
- 		return true;
+ 	if(this.page<this.menuList.length){
+ 		callback();
  	}else{
  		return false;
  	}
